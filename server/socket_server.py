e40c2af889c50f1e2afc142062530f1e767274ca
dustinkeller <dustin.w.keller@gmail.com>
2024-03-17 21:57:19 -0400

    adding back comments and updated requirements

--- server/socket_server.py
+++ server/socket_server.py
@@ -1,19 +1,29 @@
+from aiohttp import web
 import socketio
 
-sio = socketio.Client()
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+async def index(request):
+    """Serve the client-side application."""
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
 @sio.event
-def connect():
-    print('connection established')
+def connect(sid, environ):
+    print("connect ", sid)
 
 @sio.event
-def my_message(data):
-    print(f'message received with {data}')
-    sio.emit('my response', {'response': 'my response'})
+async def chat_message(sid, data):
+    print("message ", data)
 
 @sio.event
-def disconnect():
-    print('disconnected from server')
+def disconnect(sid):
+    print('disconnect ', sid)
 
-sio.connect('http://localhost:5000')
-sio.wait()
+# app.router.add_static('/static', 'static')
+# app.router.add_get('/', index)
+
+if __name__ == '__main__':
+    web.run_app(app)
